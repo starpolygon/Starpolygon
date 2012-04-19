afc3471714f280dd7bbb666ad8f0fc54ad3a074a
Brendan Opalka <b.opalka@gmail.com>
2012-04-19 16:21:46 -0400

    adde variables
    Change-Id: I6705ff570f23174a260c2b39e7ec18193f557639

--- PrjStarPoly/src/Frame.java
+++ PrjStarPoly/src/Frame.java
@@ -2,39 +2,45 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Frame extends JFrame {
-	
+
 	public JLabel textlabel1;
 	public JLabel textlabel2;
 	public JLabel textlabel3;
-	
-	
-	public Frame (){
+	//variables
+	int r = 100;
+	int X = 220;
+	int Y = 170;
+	int place = 0;
+	float[] xc;
+	float[] yc;
+	float[] linex;
+	float[] liney;
+
+	public Frame() {
 		super("Star Polygon Project");
 		setLayout(new FlowLayout());
-		
-		//Dots field
-		JLabel textlabel1 = new JLabel("Enter Number of dots:",JLabel.LEFT);
-		textlabel1.setToolTipText("Enter the number of dots you would like to have appear in a circle");
+
+		// Dots field
+		JLabel textlabel1 = new JLabel("Enter Number of dots:", JLabel.LEFT);
+		textlabel1
+				.setToolTipText("Enter the number of dots you would like to have appear in a circle");
 		add(textlabel1);
-		TextField numdots = new TextField(10); 
+		TextField numdots = new TextField(10);
 		add(numdots);
-		
-		//Milliseconds field
+
+		// Milliseconds field
 		// stuff
-		JLabel textlabel2 = new JLabel("Speed in Milliseconds:",JLabel.LEFT);
+		JLabel textlabel2 = new JLabel("Speed in Milliseconds:", JLabel.LEFT);
 		add(textlabel2);
-		TextField speed = new TextField(10); 
+		TextField speed = new TextField(10);
 		add(speed);
-		
-		//Steps field 
-		JLabel textlabel3 = new JLabel("Enter number of Steps:",JLabel.LEFT);
+
+		// Steps field
+		JLabel textlabel3 = new JLabel("Enter number of Steps:", JLabel.LEFT);
 		add(textlabel3);
-		TextField steps = new TextField(10); 
+		TextField steps = new TextField(10);
 		add(steps);
-		
-		
-		
+
 	}
 
-	
 }

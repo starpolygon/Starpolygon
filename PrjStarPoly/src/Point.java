70651f358874fe44704c5252b30e5edc7df88858
Brendan Opalka <b.opalka@gmail.com>
2012-04-24 16:43:36 -0400

    added stuff to point clas point on circle class and line class

--- PrjStarPoly/src/Point.java
+++ PrjStarPoly/src/Point.java
@@ -2,6 +2,10 @@
 public class Point {
 	// point class, figures out where the points are in the circle
 	public void Point(float x, float y) {
-
+		//SolidBrush sb = new SolidBrush(Color.Black);
+        Graphics g = panel1.CreateGraphics();
+        g.FillRectangle(sb, x, y, 3, 3);
+        Thread.sleep(3000);
+        
 	}
 }

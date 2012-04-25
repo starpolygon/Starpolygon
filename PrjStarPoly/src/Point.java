7694e07f1ef6a968826963cd6a34597d5fee309d
Gary <grs087@gmail.com>
2012-04-24 21:06:53 -0400

    test

--- PrjStarPoly/src/Point.java
+++ PrjStarPoly/src/Point.java
@@ -3,7 +3,9 @@ public class Point {
 	// point class, figures out where the points are in the circle
 	public void Point(float x, float y) {
 		//SolidBrush sb = new SolidBrush(Color.Black);
-        Graphics g = panel1.CreateGraphics();
+        
+		int x =0;
+		Graphics g = panel1.CreateGraphics();
         g.FillRectangle(sb, x, y, 3, 3);
         Thread.sleep(3000);
         
